d786cf2d6859995c7600199b3710d11ff858bd87
anahm <alisnahm@gmail.com>
2011-12-07 14:33:50 -0500

    fifth commit, i think?

--- gen/com/cs50/hotpotato/R.java
+++ gen/com/cs50/hotpotato/R.java
@@ -14,18 +14,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int closeButton=0x7f050007;
         public static final int login_button=0x7f050005;
         public static final int password_text=0x7f050003;
         public static final int txt_password=0x7f050004;
         public static final int txt_username=0x7f050002;
+        public static final int username=0x7f050006;
         public static final int username_text=0x7f050001;
         public static final int welcome_text=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int showgame=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int live_folder_name=0x7f040002;
     }
 }
